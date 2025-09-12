fc054a6bc4ffc30599c4228e18df4efe91356736
Timo Pertilä <timo.pertila@forwardforever.com>
2025-09-12 23:01:19 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify ,render_template
- 
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/ModelThree/*": {"origins": "https://tamer.work"}}, supports_credentials=True)
 
 
 # Store logs in memory for demonstration
