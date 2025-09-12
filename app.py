c806412fa2aa304698bd4fa9730f65005d4434e7
Timo Pertilä <timo.pertila@forwardforever.com>
2025-09-12 21:58:55 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # allow cross-origin requests
-
+ 
 @app.route('/api', methods=['POST'])
 def handle_post():
     return jsonify({"status": "success", "message": "POST request received"})
