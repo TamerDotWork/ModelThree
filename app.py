04d967df31361b97bc817d8d88bb9d27918e9cc7
Timo Pertilä <timo.pertila@forwardforever.com>
2025-09-13 10:58:25 +0400

    modified:   app.py

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
-from flask import Flask, request, jsonify ,render_template
-from flask_cors import CORS
-
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-CORS(app, resources={r"/ModelThree/*": {"origins": "https://tamer.work"}}, supports_credentials=True)
 
-
-# Store logs in memory for demonstration
-logs = []
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/api', methods=['GET', 'POST'])
+@app.route('/api', methods=['POST'])
 def api():
-    if request.method == 'POST':
- 
-        print("logs")
-
-    
-    # GET request: return logs
-    return jsonify({"status": "success", "logs": logs})
+    data = request.get_json()  # receive JSON from AJAX
+    print("Received:", data)   # print to terminal
+    return jsonify({"message": "Hello, World!"})
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5003)
